00e61fee4dcb2c3d3887508bb35cc50b84ae7ce9
Ian Marcelino <iandevcontato@gmail.com>
2025-06-18 19:16:43 -0300

    add user lucas

--- lib/btags.ts
+++ lib/btags.ts
@@ -2,4 +2,5 @@ export const USER_BTAG_DICT: Record<string, string> = {
   '6628e758-73bf-463b-bd9a-93b5f6447205': 'sunne01',
   '82c69510-f9e4-4acc-b32e-6933059165f5': '123456',
   'b1125dc5-5619-4c3e-94f4-efd40516e56d': '123456',
+  '8e366478-a242-4d56-891e-28986bdc94a9': '123456',
 }
